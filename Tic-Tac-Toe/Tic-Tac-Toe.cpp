69912a06774213086fd4d8f16f435dda8e8ba35b
GregVido <gregvido17@gmail.com>
2026-09-04 08:33:05 +0200

    🎨 Réindentation et gestion du clic gauche dans WndProc
    Le code a été réindenté avec des tabulations pour une meilleure lisibilité, sans modification fonctionnelle dans wWinMain, MyRegisterClass, InitInstance et About. Dans WndProc, la gestion du message WM_LBUTTONDOWN a été ajoutée : lors d’un clic gauche, la position de la souris est récupérée, la case correspondante identifiée et, si elle est vide, un symbole aléatoire (X ou O) y est placé avant de redessiner la fenêtre. Les autres messages restent inchangés, hormis la mise en forme.

--- Tic-Tac-Toe/Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe/Tic-Tac-Toe.cpp
@@ -33,41 +33,41 @@ LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Placez le code ici.
+	// TODO: Placez le code ici.
 
-    // Initialise les chaînes globales
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_TICTACTOE, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	// Initialise les chaînes globales
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_TICTACTOE, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
-    // Effectue l'initialisation de l'application :
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// Effectue l'initialisation de l'application :
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TICTACTOE));
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TICTACTOE));
 
-    MSG msg;
+	MSG msg;
 
-    // Boucle de messages principale :
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+	// Boucle de messages principale :
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
-    return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 
@@ -79,23 +79,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TICTACTOE));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_TICTACTOE);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TICTACTOE));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_TICTACTOE);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+	return RegisterClassExW(&wcex);
 }
 
 //
@@ -110,31 +110,31 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Stocke le handle d'instance dans la variable globale
+	hInst = hInstance; // Stocke le handle d'instance dans la variable globale
 
-   HWND hWnd = CreateWindowW(
-       szWindowClass,
-       szTitle,
-       WS_OVERLAPPEDWINDOW,
-       CW_USEDEFAULT,
-       CW_USEDEFAULT,
-       700,
-       700,
-       nullptr,
-       nullptr,
-       hInstance,
-       nullptr
-   );
+	HWND hWnd = CreateWindowW(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		700,
+		700,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr
+	);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
-   return TRUE;
+	return TRUE;
 }
 
 //
@@ -149,96 +149,137 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Analyse les sélections de menu :
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hWnd, &ps);
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Analyse les sélections de menu :
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
 
-        // Récupère la taille intérieure de la fenêtre
-        RECT clientRect;
-        GetClientRect(hWnd, &clientRect);
+		// Récupère la taille intérieure de la fenêtre
+		RECT clientRect;
+		GetClientRect(hWnd, &clientRect);
 
-        int windowWidth = clientRect.right - clientRect.left;
-        int windowHeight = clientRect.bottom - clientRect.top;
+		int windowWidth = clientRect.right - clientRect.left;
+		int windowHeight = clientRect.bottom - clientRect.top;
 
-        // Position du plateau pour le centrer
-        int boardX = (windowWidth - BOARD_SIZE) / 2;
-        int boardY = (windowHeight - BOARD_SIZE) / 2;
+		// Position du plateau pour le centrer
+		int boardX = (windowWidth - BOARD_SIZE) / 2;
+		int boardY = (windowHeight - BOARD_SIZE) / 2;
 
-        // Bordure extérieure
-        Rectangle(
-            hdc,
-            boardX,
-            boardY,
-            boardX + BOARD_SIZE,
-            boardY + BOARD_SIZE
-        );
+		// Bordure extérieure
+		Rectangle(
+			hdc,
+			boardX,
+			boardY,
+			boardX + BOARD_SIZE,
+			boardY + BOARD_SIZE
+		);
 
-        // Lignes verticales
-        for (int i = 1; i < 3; i++)
-        {
-            int x = boardX + i * CELL_SIZE;
+		// Lignes verticales
+		for (int i = 1; i < 3; i++)
+		{
+			int x = boardX + i * CELL_SIZE;
 
-            MoveToEx(hdc, x, boardY, nullptr);
-            LineTo(hdc, x, boardY + BOARD_SIZE);
-        }
+			MoveToEx(hdc, x, boardY, nullptr);
+			LineTo(hdc, x, boardY + BOARD_SIZE);
+		}
 
-        // Lignes horizontales
-        for (int i = 1; i < 3; i++)
-        {
-            int y = boardY + i * CELL_SIZE;
+		// Lignes horizontales
+		for (int i = 1; i < 3; i++)
+		{
+			int y = boardY + i * CELL_SIZE;
 
-            MoveToEx(hdc, boardX, y, nullptr);
-            LineTo(hdc, boardX + BOARD_SIZE, y);
-        }
+			MoveToEx(hdc, boardX, y, nullptr);
+			LineTo(hdc, boardX + BOARD_SIZE, y);
+		}
 
-        EndPaint(hWnd, &ps);
-    }
-    break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_LBUTTONDOWN:
+	{
+		// Position de la souris
+		int mouseX = LOWORD(lParam);
+		int mouseY = HIWORD(lParam);
+
+		// Taille actuelle de la fenêtre
+		RECT clientRect;
+		GetClientRect(hWnd, &clientRect);
+
+		int windowWidth = clientRect.right - clientRect.left;
+		int windowHeight = clientRect.bottom - clientRect.top;
+
+		// Position du plateau
+		int boardX = (windowWidth - BOARD_SIZE) / 2;
+		int boardY = (windowHeight - BOARD_SIZE) / 2;
+
+		// Vérifie si on a cliqué à l'intérieur du plateau
+		if (mouseX >= boardX &&
+			mouseX < boardX + BOARD_SIZE &&
+			mouseY >= boardY &&
+			mouseY < boardY + BOARD_SIZE)
+		{
+			// Trouve la colonne et la ligne
+			int column = (mouseX - boardX) / CELL_SIZE;
+			int row = (mouseY - boardY) / CELL_SIZE;
+
+			// Seulement si la case est vide
+			if (board[row][column] == 0)
+			{
+				// X ou O aléatoire
+				board[row][column] = randomSymbol(randomGenerator);
+
+				// Demande à Windows de redessiner la fenêtre
+				InvalidateRect(hWnd, nullptr, TRUE);
+			}
+		}
+
+		break;
+	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
 // Gestionnaire de messages pour la boîte de dialogue À propos de.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
 }
