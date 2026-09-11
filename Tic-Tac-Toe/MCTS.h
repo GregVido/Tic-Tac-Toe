30f01f97fd40c2699721916d50849334ce9733dd
GregVido <gregvido17@gmail.com>
2026-09-11 13:49:10 +0200

    ✨ Ajout de MCTS (Monte Carlo Tree Search) au projet
    Ajout des fichiers MCTS.h et MCTS.cpp pour implémenter l'algorithme MCTS dans le jeu Tic-Tac-Toe. Déclaration de la fonction FindMCTSMove dans MCTS.h pour déterminer un coup IA. Mise à jour des fichiers .vcxproj et .vcxproj.filters pour inclure ces nouveaux fichiers dans les groupes appropriés. Inclusion de "MCTS.h" dans MCTS.cpp.

---
+++ Tic-Tac-Toe/MCTS.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <utility>
+
+// Retourne {row, column}
+// {-1, -1} si aucun coup n'est possible
+std::pair<int, int> FindMCTSMove(
+	const int board[3][3],
+	int aiPlayer,
+	int iterations = 200
+);
