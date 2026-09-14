289b597f9920ffec5babb49ceb5b378d1a31aae2
GregVido <gregvido17@gmail.com>
2026-09-14 19:38:44 +0200

    ✨ Remplace MinimaxAI par FindBestMinimaxMove
    La classe MinimaxAI a été supprimée et remplacée par l’inclusion de <utility> et la déclaration de la fonction FindBestMinimaxMove. Cette fonction retourne une paire d’entiers indiquant la meilleure position pour l’IA sur un plateau 3x3, ou {-1, -1} si aucun coup n’est possible.

--- Tic-Tac-Toe/MinimaxAI.h
+++ Tic-Tac-Toe/MinimaxAI.h
@@ -1,5 +1,10 @@
 #pragma once
-class MinimaxAI
-{
-};
 
+#include <utility>
+
+// Retourne {ligne, colonne} du meilleur coup.
+// {-1, -1} si aucun coup possible.
+std::pair<int, int> FindBestMinimaxMove(
+	const int board[3][3],
+	int aiPlayer
+);
