605927d57dc42e14edc65c60ab211c49519bd6f6
GregVido <gregvido17@gmail.com>
2026-09-15 10:18:34 +0200

    ✨ Refactorisation et ajouts pour gestion du menu
    Ajout de l’énumération MenuHoverTarget pour le survol des éléments du menu. Harmonisation des signatures de GetMenuLayout et GetEndGameLayout. DrawMenuButton accepte un paramètre optionnel hovered. DrawMenu prend la hauteur de fenêtre en paramètre. Nouvelles fonctions pour gérer le survol et l’animation du menu : UpdateMenuHover, ClearMenuHover, ResetMenuAnimation, TickMenuAnimation.

--- Tic-Tac-Toe/Menu.h
+++ Tic-Tac-Toe/Menu.h
@@ -6,14 +6,12 @@ struct MenuLayout
 {
 	RECT j1Human;
 	RECT j1Bot;
-
 	RECT j1Easy;
 	RECT j1Medium;
 	RECT j1Hard;
 
 	RECT j2Human;
 	RECT j2Bot;
-
 	RECT j2Easy;
 	RECT j2Medium;
 	RECT j2Hard;
@@ -27,22 +25,37 @@ struct EndGameLayout
 	RECT changePlayersButton;
 };
 
-MenuLayout GetMenuLayout(int windowWidth);
+enum class MenuHoverTarget
+{
+	None,
+	J1Human,
+	J1Bot,
+	J1Easy,
+	J1Medium,
+	J1Hard,
+	J2Human,
+	J2Bot,
+	J2Easy,
+	J2Medium,
+	J2Hard,
+	Start
+};
 
-EndGameLayout GetEndGameLayout(
-	int windowWidth,
-	int boardY
-);
+MenuLayout GetMenuLayout(int windowWidth);
+EndGameLayout GetEndGameLayout(int windowWidth, int boardY);
 
 void DrawMenuButton(
 	HDC hdc,
 	const RECT& rect,
 	const wchar_t* text,
 	bool selected,
-	COLORREF selectedColor
+	COLORREF selectedColor,
+	bool hovered = false
 );
 
-void DrawMenu(
-	HDC hdc,
-	int windowWidth
-);
+void DrawMenu(HDC hdc, int windowWidth, int windowHeight);
+
+bool UpdateMenuHover(POINT mousePoint, int windowWidth);
+void ClearMenuHover();
+void ResetMenuAnimation();
+bool TickMenuAnimation();
